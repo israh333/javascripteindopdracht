94a524b2520c08661d82f154d32f0da191a17dba
Israh <israh.emara@coderclass.nl>
2017-10-19 10:30:09 +0200

    Een stuiterend balletje gemaakt nu bezig met 3 balletjes

--- jsvoorgames.js
+++ jsvoorgames.js
@@ -1,26 +1,47 @@
-var xCar = 50; //Meest links boven
-var yCar = 100; //Meest links boven
-var xSize = 200;
-var ySize = 180;
-var wheelSize = 40;
+function setup(){
+  createCanvas(400,400);
+  balletje = new Ball;
 
-function setup() {
-  createCanvas(xSize, ySize);
-  background(220);
 }
 
+
+function Ball(xx,yy){
+  this.x = xx;
+  this.y = yy;
+  this.speedx = 2;
+  this.speedy = 3;
+
+}
+
+var balletje = new Ball(10,20);
+var balletje2 = new Ball(1,200);
+var balletje3 = new Ball(1,300);
+
+
+
 function draw() {
-    noStroke();
-    fill(255, 220, 115);
-    rect(xCar, yCar, 110, 20);
-    rect(xCar + 10, yCar-22, 70, 40);
-    // Wielen
-    stroke(255);
-    strokeWeight(2);
-    fill(12, 66, 66);
-    ellipse(xCar + 25, yCar + 21, wheelSize, wheelSize);
-    ellipse(xCar + 80, yCar + 21, wheelSize, wheelSize);
-    stroke(0); //Zwarte weg
-    line(0, yCar + 21 + wheelSize/2, width, yCar + 21 + wheelSize/2);
-    //'width' is de breedte van je canvas
-};
+  background(150)
+  rect(balletje,balletje, 10,20);
+  rect(balletje2,balletje2, 10,10);
+  rect(balletje3,balletje3, 10,10);
+
+  balletje.x += balletje.speedx1;
+  balletje.y += balletje.speedy1;
+
+rect(balletje2.x,balletje2.y ,10,10);
+  balletje.x2 += balletje.speedx2;
+  balletje.y2 += balletje.speedy2;
+
+rect(balletje3.x,balletje3.y ,10,10);
+  balletje3.x += balletje3.speedx;
+  balletje3.y += balletje3.speedy;
+
+if (balletje.x > (400-10) || balletje.y < 0)
+  balletje.speedy = -balletje.speedy;
+
+if (balletje2.x > (400-10) || balletje2.x< 0)
+  balletje2.speedx = -balletje2.speedx;
+
+if (balletje3.x > (400-10) || balletje3.x < 0)
+  balletje3.speedx = -balletje3.speedx;
+}

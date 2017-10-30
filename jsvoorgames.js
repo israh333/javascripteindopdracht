a0e8598a658d2dc7fc812cf3443541e38d5beeef
Israh <israh.emara@coderclass.nl>
2017-10-30 10:28:45 +0100

    Drie balletjes geanimeerd die stuiteren

--- jsvoorgames.js
+++ jsvoorgames.js
@@ -1,9 +1,6 @@
 function setup(){
   createCanvas(400,400);
-  balletje = new Ball;
-
 }
-
 
 function Ball(xx,yy){
   this.x = xx;
@@ -11,37 +8,43 @@ function Ball(xx,yy){
   this.speedx = 2;
   this.speedy = 3;
 
+  this.update = function(){
+    this.x += this.speedx;
+    this.y += this.speedy;
+
+    if (this.y > (400-10) || this.y < 0){
+      this.speedy = -this.speedy;
+
+    if (this.x > (400-10) || this.x < 0){
+      this.speedx = -this.speedx;
+
+
+  }
+  }
+  this.teken = function () {
+
+  }
+
+ }
+
+
+
 }
 
 var balletje = new Ball(10,20);
 var balletje2 = new Ball(1,200);
-var balletje3 = new Ball(1,300);
+var balletje3 = new Ball(1,100);
 
 
 
 function draw() {
-  background(150)
-  rect(balletje,balletje, 10,20);
-  rect(balletje2,balletje2, 10,10);
-  rect(balletje3,balletje3, 10,10);
+  background(200);
+  rect(balletje.x,balletje.y, 10,10);
+  rect(balletje2.x,balletje2.y, 10,10);
+  rect(balletje3.x,balletje3.y, 10,10);
+  balletje.update();
+  balletje2.update();
+  balletje3.update();
 
-  balletje.x += balletje.speedx1;
-  balletje.y += balletje.speedy1;
 
-rect(balletje2.x,balletje2.y ,10,10);
-  balletje.x2 += balletje.speedx2;
-  balletje.y2 += balletje.speedy2;
-
-rect(balletje3.x,balletje3.y ,10,10);
-  balletje3.x += balletje3.speedx;
-  balletje3.y += balletje3.speedy;
-
-if (balletje.x > (400-10) || balletje.y < 0)
-  balletje.speedy = -balletje.speedy;
-
-if (balletje2.x > (400-10) || balletje2.x< 0)
-  balletje2.speedx = -balletje2.speedx;
-
-if (balletje3.x > (400-10) || balletje3.x < 0)
-  balletje3.speedx = -balletje3.speedx;
 }
